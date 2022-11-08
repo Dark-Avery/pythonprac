93952907f1dfe8f290478144dbdf63570ca823d3
Dark-Avery <ingv0rr@yandex.ru>
2022-11-08 12:01:35 +0300

    .:

---
+++ 20221108/1/prog.py
@@ -0,0 +1,15 @@
+import collections
+
+
+class DivStr(collections.Userstring):
+    def __init__(self, seq=""):
+        super().__init__(seq)
+
+    def __floordiv__(self, other):
+        n = len(self) // other
+        return iter(self[k:k + n] for k in range(0, other * n, n))
+
+    def __mod__(self, other):
+        n = len(self) % other
+        return self[-n:0]
+
